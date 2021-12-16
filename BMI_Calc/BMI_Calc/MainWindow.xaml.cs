1dc3f838d755d24f4e2a38727e0a323178b8434c
afusco059 <70164839+afusco059@users.noreply.github.com>
2021-12-16 10:09:08 -0500

    Add files via upload

---
+++ BMI_Calc/BMI_Calc/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BMI_Calc
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void Exit_btn(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Clear_btn(object sender, RoutedEventArgs e)
+        {
+            First_Name.Text = "";
+            Last_Name.Text = "";
+            Phone_num.Text = "";
+            Height.Text = "";
+            Weight.Text = "";
+        }
+    }
+}
